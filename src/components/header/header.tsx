d70efbb304541b2a58cf90f0e531048abc47ad07
iceyisaak <me@iceyisaak.com>
2026-03-13 16:07:58 +0100

    Added styling for Hero Section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,20 @@
 import { getRecommendedMenu } from "../../apis/recipe-api";
+import { SearchBar } from "../navbar/search-bar";
+import { VscRobot } from "react-icons/vsc";
 
 export const Header = () => {
   const { data, error, isLoading } = getRecommendedMenu();
   return (
     <header>
-      <h3>Recommended Menu:</h3>
+      <aside className="w-full h-[50rem] bg-blue-200 flex flex-col justify-center items-center">
+        <h3 className="text-5xl mb-10">Search Our Recommended Recipes</h3>
+        <SearchBar />
+        <p className="mt-8 mb-4">or Chat with Our</p>
+        <button className="h-14 bg-amber-600 w-[16vw] cursor-pointer flex items-center justify-center gap-2 text-xl font-bold">
+          <VscRobot /> CHEFBOT
+        </button>
+      </aside>
+      <h3 className="text-3xl">Recommended Menu:</h3>
       {error && <h3>Error: {error.message}</h3>}
       {isLoading && <h3>Loading...</h3>}
       {data && (
--- src/components/navbar/search-bar/search-bar.tsx
+++ src/components/navbar/search-bar/search-bar.tsx
@@ -1,8 +1,13 @@
 export const SearchBar = () => {
   return (
     <section>
-      <input type="text" name="search bar" placeholder="e.g. Steak" />
-      <button>Search</button>
+      <input
+        type="text"
+        name="search bar"
+        placeholder="e.g. Steak"
+        className="bg-gray-100 w-[30vw] h-10 px-3"
+      />
+      <button className="ml-3 bg-blue-600 h-10 w-30">Search</button>
     </section>
   );
 };
