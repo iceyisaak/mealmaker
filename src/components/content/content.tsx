1f948458e8274bbf9c1604d8fa532b51b51547bc
iceyisaak <me@iceyisaak.com>
2026-03-16 12:44:38 +0100

    Cleaned up code

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,13 +1,13 @@
 import { ShowcaseCategories } from "./showcase-categories";
-import { ShowcaseIngredients } from "./showcase-ingredients";
-import { ShowcaseRegions } from "./showcase-regions";
+// import { ShowcaseIngredients } from "./showcase-ingredients";
+// import { ShowcaseRegions } from "./showcase-regions";
 
 export const Content = () => {
   return (
     <section>
       <ShowcaseCategories />
-      <ShowcaseIngredients />
-      <ShowcaseRegions />
+      {/* <ShowcaseIngredients />
+      <ShowcaseRegions /> */}
     </section>
   );
 };
