7be8c038248cf90c1cda589b863a7dc7e01550ac
iceyisaak <me@iceyisaak.com>
2026-03-16 16:18:06 +0100

    Updated styling for Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,25 @@
 export const Footer = () => {
   return (
     <footer className="h-16 bg-[#181A1B] flex flex-col justify-center">
-      <a
-        href="https://github.com/iceyisaak/mealmaker"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <h3 className="text-center">Created by Iceyisaak</h3>
-      </a>
+      <h3 className="text-center">
+        Project{" "}
+        <a
+          href="https://github.com/iceyisaak/mealmaker"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          MealMaker
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://github.com/iceyisaak/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="underline">Iceyisaak</span>
+        </a>
+      </h3>
     </footer>
   );
 };
