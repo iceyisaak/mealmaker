ba4af8841e9b6ff174f3c54934e0241754f529bc
iceyisaak <me@iceyisaak.com>
2026-03-16 15:34:49 +0100

    Resolved element width issue

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,19 +8,27 @@ export const Header = () => {
     <header className="bg-[#181A1B]">
       <SearchSection />
       <section className="py-50 px-[5vw]">
-        <h3 className="text-5xl mb-14 text-center">Recommended Menu</h3>
+        <h3 className="text-5xl mb-14 text-center text-gray-200">
+          Recommended Menu
+        </h3>
         {error && <h3>Error: {error.message}</h3>}
         {isLoading && <h3>Loading...</h3>}
         {data && (
-          <div className="flex justify-center flex-col">
-            <Link to="/meal/$id" params={{ id: data.idMeal }}>
+          <div className="flex justify-center">
+            <Link
+              to="/meal/$id"
+              params={{ id: data.idMeal }}
+              className="items-center w-fit"
+            >
               <img
                 src={data?.strMealThumb}
                 alt={data?.strMeal}
-                className="mx-auto mb-5"
+                className="mx-auto mb-5 w-fit"
               />
+              <h3 className="text-2xl text-center text-gray-200">
+                {data?.strMeal}
+              </h3>
             </Link>
-            <h3 className="text-2xl text-center">{data?.strMeal}</h3>
           </div>
         )}
       </section>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
   return (
     <nav className="w-full bg-amber-600 py-3">
       <Link className="flex w-fit" to={"/"}>
-        <Logo className={`w-8 h-8 text-amber-600 ml-3 mr-1`} />
-        <span className="text-2xl">MealMaker</span>
+        <Logo className={`w-8 h-8 text-amber-400 ml-3 mr-1`} />
+        <span className="text-2xl text-gray-100">MealMaker</span>
       </Link>
     </nav>
   );
