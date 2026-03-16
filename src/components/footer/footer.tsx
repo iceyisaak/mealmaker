b46eac05e3563d51dcc45112624bd2ffc385d9ff
iceyisaak <me@iceyisaak.com>
2026-03-16 16:13:04 +0100

    Added styling for Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,13 @@
 export const Footer = () => {
   return (
-    <footer>
-      <h3>Created by Iceyisaak</h3>
+    <footer className="h-16 bg-[#181A1B] flex flex-col justify-center">
+      <a
+        href="https://github.com/iceyisaak/mealmaker"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h3 className="text-center">Created by Iceyisaak</h3>
+      </a>
     </footer>
   );
 };
