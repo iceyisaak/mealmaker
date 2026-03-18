9bb581171cbc0c9165f741d9d8f682b71c61f45b
iceyisaak <me@iceyisaak.com>
2026-03-18 17:33:37 +0100

    Added route /category

---
+++ src/routes/category.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CategoryPage } from "../pages/catagory-page";
+
+export const Route = createFileRoute("/category")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <CategoryPage />;
+}
